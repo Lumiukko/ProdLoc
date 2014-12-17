725947daa067a180ea365ba37c22932b7fea0c46
Lumiukko <makko@brainscorch.net>
2014-12-17 22:14:51 +0100

    :new: Class: Brand

---
+++ ProdLoc/Brand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdLoc
+{
+    public class Brand
+    {
+
+        public UInt64 ID { get; private set; }
+        public String Name { get; private set; }
+        public Company Company { get; private set; }
+
+        public Brand(String name, Company company)
+        {
+            Name = name;
+            Company = company;
+        }
+
+        public Brand(UInt64 id, String name, Company company)
+        {
+            ID = id;
+            Name = name;
+            Company = company;
+        }
+
+        public override String ToString()
+        {
+            return string.Format("Brand: [ID={0}, Name=\"{1}\", Company={2}]", ID, Name, Company);
+        }
+
+    }
+}
