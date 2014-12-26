b9f27c316d38638630047d74c7d9b6832a5c723e
Lumiukko <makko@brainscorch.net>
2014-12-26 21:42:15 +0100

    Changed accuracy to int
    Accuracy will be rounded up in order to save space in the database. Only
    one entry per long/lat/accuracy. Perhaps the coordinates also have to be
    limited later.

--- ProdLoc/DataStructure/GeoLocation.cs
+++ ProdLoc/DataStructure/GeoLocation.cs
@@ -11,21 +11,26 @@ namespace ProdLoc
         public Int64 ID { get; private set; }
         public Double Longitude { get; private set; }
         public Double Latitude { get; private set; }
-        public float Accuracy { get; private set; }
+        public int Accuracy { get; private set; } 
 
-        public GeoLocation(Double longitude, Double latitude, float accuracy)
+        public GeoLocation(Double longitude, Double latitude, int accuracy)
         {
             Longitude = longitude;
             Latitude = latitude;
             Accuracy = accuracy;
         }
 
-        public GeoLocation(Int64 id, Double longitude, Double latitude, float accuracy)
+        public GeoLocation(Int64 id, Double longitude, Double latitude, int accuracy)
         {
             ID = id;
             Longitude = longitude;
             Latitude = latitude;
             Accuracy = accuracy;
+        }
+
+        public override String ToString()
+        {
+            return string.Format("GeoLocation: [ID={0}, Longitude=\"{1}\", Latitude={2}, Accuracy={3}]", ID, Longitude, Latitude, Accuracy);
         }
     }
 }
