c4bb34f33bb0b09c68ae9b74f53e5c8ff71f76c0
Lumiukko <makko@brainscorch.net>
2014-12-27 20:16:36 +0100

    Attempted IsConvex Method, but failed... :confused: :question:
    Somehow the angles are messed up, given 3 coordinates, which make a
    triangle, according to google maps, one angle is nearly 180° and the
    other two almost 0° ...

--- ProdLoc/DataStructure/GeoPolygon.cs
+++ ProdLoc/DataStructure/GeoPolygon.cs
@@ -22,7 +22,25 @@ namespace ProdLoc
         /// <returns>True if the polygon is convex, or False if it is not.</returns>
         public bool IsConvex()
         {
-            throw new NotImplementedException();
+            List<GeoLocation> locList = new List<GeoLocation>();
+            locList.Add(Vertices.Last());
+            locList.AddRange(Vertices);
+            locList.Add(Vertices.First());
+            for (int i = 1; i < locList.Count - 1; i++) 
+            {
+                double dLeft = locList.ElementAt(i).Distance(locList.ElementAt(i - 1));
+                double dRight = locList.ElementAt(i).Distance(locList.ElementAt(i + 1));
+                double dCenter = locList.ElementAt(i - 1).Distance(locList.ElementAt(i + 1));
+                double angle = Math.Acos((dLeft * dLeft + dRight * dRight - dCenter * dCenter) / (2 * dLeft * dRight));
+                angle *= (180 / Math.PI);
+                //TODO: Something is FUBAR here, the angles are completely messed up...
+                Console.WriteLine(string.Format("Angle: {0}\n\tdLeft: {1}\n\tdRight: {2}\n\tdCenter: {3}", angle, dLeft, dRight, dCenter));
+                if (angle > 180)
+                {
+                    //return false;  // If an internal angle is larger than 180°, this cannot be a convex polygon.
+                }
+            }
+            return true;
         }
 
 
@@ -54,7 +72,7 @@ namespace ProdLoc
                 acc += Vertices.ElementAt(i).Longitude * Vertices.ElementAt(i + 1).Latitude - Vertices.ElementAt(i + 1).Longitude * Vertices.ElementAt(i).Latitude;
             }
             acc += Vertices.Last().Longitude * Vertices.First().Latitude - Vertices.First().Longitude * Vertices.Last().Latitude;
-            return 110 * 1000 * 1000 * Math.Abs(acc) / 2; // Aproximation of longitude/latitude degrees to meters in the european area.
+            return 110 * 1000 * 1000 * Math.Abs(acc) / 2; // Aproximation of longitude/latitude degrees to meters in the central european area.
         }
 
 
